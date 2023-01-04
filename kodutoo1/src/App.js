cb1303f08fc877f15838098e935eff2b3dabddc1
joosepj <joosep.joonas@gmail.com>
2023-01-04 20:49:28 +0200

    kodutoo 1 tehtud

---
+++ kodutoo1/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="tekst">Guugelda CSS-i (märksõnadega näiteks: „css make text underline“ / „css align text right“ / „css
+        change … „ /</div>
+      <button className="nupp">OK</button>
+      <img className="pilt" src="https://blog.hubspot.com/hubfs/image8-2.jpg" alt=""/>
+    </div>
+  );
+}
+
+export default App;
