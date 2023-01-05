86d11ae3c944b1fd3b287a36e9f4b2c4066ee901
joosepj <joosep.joonas@gmail.com>
2023-01-05 16:12:14 +0200

    useState, useRef, dünaamiline väljakuvamine

--- veebipood/src/pages/Avaleht.js
+++ veebipood/src/pages/Avaleht.js
@@ -1,14 +1,39 @@
 import { useState } from "react";
 
 function Avaleht() {
-  const [kogus, muudaKogus] = useState();
+  const [kogus, muudaKogus] = useState(0);  // numbrid - nendega saan teha arvutusi / suurem / väiksem
+  const [sonum, muudaSonum] = useState(""); // sõnad - suurteks/väikesteks/mis on esimene
+  const [laigitud, muudaLaigitud] = useState(false); //kahendväärtus - true või false
   
+  function nulli() {
+    muudaKogus(0);
+    muudaSonum("Kogus nullitud");
+  }
+
+  function vahenda() {
+    muudaKogus(kogus - 100);
+    muudaSonum("Kogus vähendatud");
+  }
+
+  function suurenda() {
+    muudaKogus(kogus + 100);
+    muudaSonum("Kogus suurendatud");
+
+  }
   
   return (
     <div>
-      <button onClick={() => muudaKogus(100)}>-</button>
+      { laigitud === true && <img onClick={() => muudaLaigitud(!false)} src="/laigitud.svg" alt="" />}
+      { laigitud === false && <img onClick={() => muudaLaigitud(!true)} src="/mittelaigitud.svg" alt="" />}
+      <br />
+      <button onClick={() => muudaLaigitud(!laigitud)}>Muuda laigitut</button>
+
+      {kogus > 0 && <button onClick={nulli}>Nulli</button>}
+      <br />
+      <div>{sonum}</div>
+      <button disabled={kogus === 0} onClick={vahenda}>-</button>
       <div>{kogus}</div>
-      <button onClick={() => muudaKogus(200)}>+</button>
+      <button onClick={suurenda}>+</button>
     </div>
   )
 }
--- veebipood/src/pages/LisaToode.js
+++ veebipood/src/pages/LisaToode.js
@@ -1,8 +1,26 @@
-
+import { useRef, useState } from "react"
 
 function LisaToode() {
+  const [sonum, muudaSonum] = useState("Lisa toode!");
+  const inputiLuger = useRef();
+
+  // function lisa() {} // täpselt sama mis all
+
+  const lisa = () => {
+    if (inputiLuger.current.value === "") {
+      muudaSonum("Ei saa tühja nimega toodet lisada!");
+    } else {
+      muudaSonum("Toode lisatud: " + inputiLuger.current.value);
+    }
+  }
+
   return (
-    <div>LisaToode</div>
+    <div>
+      <div>{sonum}</div>
+      <label>Uue toote nimi</label> <br />
+      <input ref={inputiLuger} type="text" /> <br />
+      <button onClick={lisa}>Lisa</button>
+    </div>
   )
 }
 
