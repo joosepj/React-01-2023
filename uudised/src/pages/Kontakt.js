5db7dea46217d1facf95e9267715d1fd4c6ec178
joosepj <joosep.joonas@gmail.com>
2023-01-10 00:26:46 +0200

    kodutöö nr.7 - uudiste projekti täiendus

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -1,13 +1,37 @@
 
-import React from 'react'
+import { useState } from "react";
 
 function Kontakt() {
-  return (<div>
-    <div>Kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
-    <div>Ära võta ühendust</div>
-    <div>Sest veebileht on veel poolik</div>
+  const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false);
+  const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false);
+  const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false);
 
-</div>)
+
+  return (
+  <div>
+    <div>Kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
+    <div>Võta meiega ühendust</div>
+    <br />
+    <div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+    <br />
+    {n2itaTelKristiine && <div>+123356</div>}
+    <br />
+    <div>Endla 45</div>
+    <br />
+    <div onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Ülemiste keskus</div>
+    <br />
+    {n2itaTelYlemiste && <div>+234566</div>}
+    <br />
+    <div>Suur-Sõjamäe 4</div>
+    <br />
+    <div onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Tasku Keskus</div>
+    <br />
+    {n2itaTelTasku && <div>+345678</div>}
+    <br />
+    <div>Turu 2</div>
+
+
+  </div>)
 }
 
 export default Kontakt
--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,8 +1,11 @@
 
-import React from 'react'
+import { useState } from "react";
 
 function Meist() {
-  return (<div>
+  const [kontakt, n2itaKontakt] = useState("");
+
+  return (
+  <div>
     <div>Meist</div>
     <br />
     <br />
@@ -10,7 +13,31 @@ function Meist() {
     <div>On proovida</div>
     <div>Kõike kasulikku</div>
     <div>Et saada aru rohkem veebiarendusest</div>
-    </div> )
+    <div>Meie töötajad:</div>
+    <br />
+    <div>Arvo Pärt</div>
+    {/* <div>+35453434</div> */}
+    <div>Uudisklippite taustamuusika</div>
+    <button onClick={() => n2itaKontakt('+35453434')}>Võta ühendust</button>
+    <br /><br />
+    <div>Kelly Sildaru</div>
+    {/* <div>+54353453</div> */}
+    <div>Püstolreporter</div>
+    <button onClick={() => n2itaKontakt('+54353453')}>Võta ühendust</button>
+    <br /><br />
+    <div>Edward Von Lõngus</div>
+    {/* <div>+463463463</div> */}
+    <div>Uudiste kujundamine</div>
+    <button onClick={() => n2itaKontakt('+463463463')}>Võta ühendust</button>
+    <div>Kerli Kõiv</div>
+    {/* <div>+3525295</div> */}
+    <div>Välisturgude spetsialist</div>
+    <button onClick={() => n2itaKontakt('+3525295')}>Võta ühendust</button>
+    <br /><br />
+    { kontakt !== "" && <div>Tema kontakt: {kontakt} </div>}
+   
+    
+  </div> )
 }
 
 export default Meist
