bbeebcaf46b94e376689d39a203ad06acc4654fa
joosepj <joosep.joonas@gmail.com>
2023-01-11 01:55:29 +0200

    kodutöö nr 8 täiendus - useState

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -4,11 +4,38 @@ import Avaleht from './pages/Avaleht';
 import Meist from './pages/Meist';
 import Kontakt from './pages/Kontakt';
 import Seaded from './pages/Seaded';
+import { useState } from 'react';
+import { useRef } from 'react';
 
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState("ei");
+  const [sonum, muudaSonum] = useState("");
+  const kasutajaNimiRef = useRef();
+  const paroolRef = useRef();
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123") {
+      muudaSisselogitud("jah");
+      muudaSonum(kasutajaNimiRef.current.value+ ", Oled sisselogitud");
+    } else {
+      muudaSonum("Vale parool");
+    }
+  }
   return (
     <div className="App">
+      <div>{sonum}</div>
+      { sisselogitud === "ei" && <div>
+       <label>Kasutajanimi</label><br />
+        <input ref={kasutajaNimiRef}type="text" /><br />
+        <label>Parool</label><br />
+        <input ref={paroolRef} type="password" /><br />
+      </div>}
+      
+      { sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+      { sisselogitud === "jah" && <button onClick={() => muudaSisselogitud("ei")}>Logi välja</button>}
+
+
       <div className="tekst">Kodutöö 1</div>
       <button className="nupp">OK</button><br />
       <img className="pilt" src="https://blog.hubspot.com/hubfs/image8-2.jpg" alt=""/>
@@ -35,4 +62,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
