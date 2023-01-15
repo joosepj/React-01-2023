560ca6565d8123db48583bd00106f8cd125a9851
joosepj <joosep.joonas@gmail.com>
2023-01-16 00:17:52 +0200

    Kodutoo nr 9 - localStorage

--- kodutoo1/src/pages/Seaded.js
+++ kodutoo1/src/pages/Seaded.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState } from 'react';
 
 function Seaded() {
+  const [kujundus, muudaKujundus] = useState(localStorage.getItem("veebilehe_kujundus"));
+
+  const tumeLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "dark_mode");
+    muudaKujundus("dark_mode");
+  }
+  
+  const heleLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "light_mode");
+    muudaKujundus("light_mode");
+  }
+
+  const punaneLeht = () => {
+    localStorage.setItem("veebilehe_kujundus", "red_mode");
+    muudaKujundus("red_mode");
+  }
+
+
+
   return (
-    <div>Seaded</div>
+    <div>
+      <button onClick={tumeLeht}>Tume leht</button>
+      <button onClick={heleLeht}>Hele leht</button>
+      <button onClick={punaneLeht}>Punane leht</button>
+      { kujundus==="dark_mode" && <div>TUME LEHT</div>}
+      { kujundus==="light_mode" && <div>HELE LEHT</div>}
+      { kujundus==="red_mode" && <div>PUNANE LEHT</div>}
+    </div>
+
   )
 }
 
