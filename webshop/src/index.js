5ff1ea4fc8a1aa58164d67cb76683269ed05019b
joosepj <joosep.joonas@gmail.com>
2023-02-20 20:33:43 +0200

    webshop - mapi lisamine

--- webshop/src/index.js
+++ webshop/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 import './i18n';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import 'leaflet/dist/leaflet.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
