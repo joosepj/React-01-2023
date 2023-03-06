a6b3a54a6395baa614d3f4bcc9cc73b80cf94bff
joosepj <joosep.joonas@gmail.com>
2023-03-07 01:40:39 +0200

    kodutöö 27b- lisauudis

--- uudised/src/pages/LisaUudis.js
+++ uudised/src/pages/LisaUudis.js
@@ -1,7 +1,9 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 function LisaUudis() {
     const uudiseRef = useRef();
+    const [s6num, uuendaS6num] = useState("TEST");
+
 
     const lisaUusUudis = () => {
         let uudised = localStorage.getItem("uudised");
@@ -9,12 +11,27 @@ function LisaUudis() {
         uudised.push(uudiseRef.current.value);
         uudised = JSON.stringify(uudised);
         localStorage.setItem("uudised", uudised);
+
+
+    }
+
+    const kontrolli = () => {
+      uuendaS6num("");
+
+      if (uudiseRef.current.value.charAt(0) === uudiseRef.current.value.charAt(0).toLowerCase()){
+        uuendaS6num("Sisestasid uudise väikse tähega, paranda")
+      }
+    
+      if (uudiseRef.current.value.includes("  ")) {
+        uuendaS6num("Sisestasid kaks tühikut, paranda")
+      }
     }
 
   return (
     <div>
+        <div>{s6num}</div>
         <label>Uudise nimi</label><br />
-        <input ref={uudiseRef} type="text" name="" /><br />
+        <input onChange={kontrolli} ref={uudiseRef} type="text" name="" /><br />
         <button onClick= {() => lisaUusUudis()}>Lisa uudis</button>
 
     </div>
