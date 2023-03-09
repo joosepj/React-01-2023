7193ff1d30e3e6fad9e83e7e41aa8d64c524fc81
joosepj <joosep.joonas@gmail.com>
2023-03-09 16:50:10 +0200

    Kodutöö 29 - uudised, api

--- uudised/src/pages/Avaleht.js
+++ uudised/src/pages/Avaleht.js
@@ -1,12 +1,29 @@
-
-import React from 'react'
+import { useEffect, useState } from "react";
 
 function Avaleht() {
+  const [postitused, uuendaPostitused] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => uuendaPostitused(data))
+  }, []);
+
+
+
   return (
-<div>
+  <div>
     <div>Avaleht</div>
     <img src="https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80" alt="" />
-</div>  )
+    {postitused.map(element =>
+      <div>
+       <div><u>{element.userId}</u></div>
+       <div><b>{element.id}</b></div>
+       <div>{element.title}</div>
+       <div>{element.body}</div>
+      </div>
+       )}
+  </div>  );
 }
 
 export default Avaleht
