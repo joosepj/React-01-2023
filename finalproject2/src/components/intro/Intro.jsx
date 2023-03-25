026199e0ebe30140355726c89db1771b9097fd76
joosepj <joosep.joonas@gmail.com>
2023-03-25 04:41:11 +0200

    uue projekti algus, esimene lehe osa tehtud

---
+++ finalproject2/src/components/intro/Intro.jsx
@@ -0,0 +1,34 @@
+import "./intro.css";
+
+function Intro() {
+  return (
+    <div className="i">
+        <div className="i-left">
+            <div className="i-left-wrapper">
+                <h1 className="i-header">Joosepi reis Roomasse</h1>
+                <h2 className="i-date">2023 Veebruar</h2>
+                <div className="i-title">
+                    <div className="i-title-wrapper">
+                        <div className="i-title-item">10000 sammu</div>
+                        <div className="i-title-item">110km läbitud jalgsi</div>
+                        <div className="i-title-item">11sdgsgdsdgsi</div>
+                        <div className="i-title-item">1fsdfgsöfsdöflgsi</div>
+                        <div className="i-title-item">1ösdöfsdöfsalgsi</div>
+                    </div>
+                </div>
+                <div className="i-desc">
+                    Joosepi reis Roomasse Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Facilis architecto praesentium explicabo autem temporibus, 
+                    ea tempora molestiae rem eligendi beatae perferendis magni quis corporis nisi ducimus, 
+                    aspernatur repudiandae ratione totam?
+                </div>
+            </div>
+        </div>
+        <div className="i-right">
+            <div className="i-bg"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
