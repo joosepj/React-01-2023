91b553ff2253c976a1c99b9ec7b78b2616ef47ef
joosepj <joosep.joonas@gmail.com>
2023-03-26 06:25:52 +0300

    lõpuprojekt - piltidega jändamine

--- finalproject2/src/App.jsx
+++ finalproject2/src/App.jsx
@@ -1,9 +1,11 @@
+import Gallery from "./components/gallery/Gallery";
 import Intro from "./components/intro/Intro";
 
 function App() {
   return (
     <div>
       <Intro />
+      <Gallery />
     </div>
   );
 }
