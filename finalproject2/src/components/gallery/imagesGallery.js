7ac2a259166ae8ceb15e3e8a9c68151d024d6be9
joosepj <joosep.joonas@gmail.com>
2023-03-28 21:40:42 +0300

    firebase ülesse laadimine

--- finalproject2/src/components/gallery/imagesGallery.js
+++ finalproject2/src/components/gallery/imagesGallery.js
@@ -1,16 +1,34 @@
 const images = [
     {
-        src: "https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1581%20Large.jpeg?alt=media&token=941cb37e-a0ac-4a29-bbfc-e44c12a9d835",
+        src: "https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1206.jpeg?alt=media&token=2761db63-90b8-484b-aec1-02e4b31e008e",
     },
     {
-        src: "",
+        src: "https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1220.jpeg?alt=media&token=6efb609a-b439-49e3-be6b-653f3e4013db",
     },
     {
-        src: "",
+        src: "https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1227.jpeg?alt=media&token=e6255ff5-9ffc-4ecb-9415-19951bf6da31",
     },
     {
-        src:"",
-    }
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1243.jpeg?alt=media&token=0b2fbdbd-a38c-4587-88bc-a5c9ddb4b192",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1278.jpeg?alt=media&token=121fa3b7-abb8-4bf2-b23d-bb8dc722f1c2",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1327.jpeg?alt=media&token=63a3045d-da0b-4cc5-9f43-48df2ee2d969",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1460.jpeg?alt=media&token=86d7540b-e9c0-4533-a8c1-4c33b1250389",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1475.jpeg?alt=media&token=c1df93a3-912e-4594-bdbe-cfa30735332c",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1551.jpeg?alt=media&token=937712bf-a0ae-4d47-8e0c-335c65c9542f",
+    },
+    {
+        src:"https://firebasestorage.googleapis.com/v0/b/finalproject2-b8e8b.appspot.com/o/IMG_1585.jpeg?alt=media&token=345460de-06c1-4526-81bf-e39439479834",
+    },
 
 ];
 
